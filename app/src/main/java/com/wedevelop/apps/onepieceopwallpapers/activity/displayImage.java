b1c525f1338094b467dd804d7c3336e73cafa638
rawkush <ankushrawat950@gmail.com>
2018-06-28 15:54:16 +0530

    few minor edits

--- app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/displayImage.java
+++ app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/displayImage.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.wedevelop.apps.onepieceopwallpapers.R;
 import com.wedevelop.apps.onepieceopwallpapers.models.Wallpaper;
@@ -18,6 +19,7 @@ public class displayImage extends AppCompatActivity {
     FloatingActionButton fab_more, fab_download, fab_set_wall,fab_share; // fab buttons on layout
     Animation OpenAnimation,CloseAnimation,clockwiseAnimation,AnticlockwiseAnimation;
     Boolean isOpen= false;
+    LinearLayout LinearFabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,8 @@ public class displayImage extends AppCompatActivity {
         fab_download=(FloatingActionButton) findViewById(R.id.fab_download);
         fab_set_wall=(FloatingActionButton) findViewById(R.id.fab_set_wall);
         fab_share=(FloatingActionButton)findViewById(R.id.fab_set_wall);
+        LinearFabLayout=(LinearLayout)findViewById(R.id.LinearFablayout);
+
         //initialising the animation variable
         OpenAnimation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
 
@@ -50,10 +54,12 @@ public class displayImage extends AppCompatActivity {
                     fab_share.setClickable(false);
                     fab_set_wall.setClickable(false);
                     fab_download.setClickable(false);
+                    LinearFabLayout.setVisibility(0);
                     isOpen=false;
 
                 }
                 else{
+                    LinearFabLayout.setVisibility(1);
                     fab_download.startAnimation(OpenAnimation);
                     fab_set_wall.startAnimation(OpenAnimation);
                     fab_more.startAnimation(clockwiseAnimation);
