d9ac3f7244f889fc33f9c22ef90726ec80484050
Ahraar <ahraralam2012@gmail.com>
2018-08-12 16:25:48 +0530

    fullscreen added

--- app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/DisplayImage.java
+++ app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/DisplayImage.java
@@ -16,6 +16,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
@@ -61,6 +63,10 @@ public class DisplayImage extends AppCompatActivity implements CompoundButton.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_display_image);
         fab_more = findViewById(R.id.fab_more);
         fab_download = findViewById(R.id.fab_download);
