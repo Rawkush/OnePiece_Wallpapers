1e10b09c3e2004dae359bf03f5cf0e415646fbeb
Ahraar <ahraralam2012@gmail.com>
2018-08-21 22:36:46 +0530

    progressbar added

--- app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/adapter/WallpaperAdapter.java
+++ app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/adapter/WallpaperAdapter.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,10 +21,15 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -30,6 +37,7 @@ import com.google.android.gms.ads.InterstitialAd;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.victor.loading.rotate.RotateLoading;
 import com.wedevelop.apps.onepieceopwallpapers.R;
 import com.wedevelop.apps.onepieceopwallpapers.activity.DisplayImage;
 import com.wedevelop.apps.onepieceopwallpapers.models.Wallpaper;
@@ -44,7 +52,7 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.wall
     private Context mCtx;
     private List<Wallpaper> wallpaperList;
     private String wallpaper;
-
+    public RotateLoading rotateLoading;
     private InterstitialAd mInterstitialAd;
 
     public WallpaperAdapter(Context mCtx, List<Wallpaper> wallpaperList) {
@@ -77,6 +85,19 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.wall
         Wallpaper w = wallpaperList.get(position);
         Glide.with(mCtx)
                 .load(w.url)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        rotateLoading.start();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        rotateLoading.start();
+                        return false;
+                    }
+                })
                 .into(holder.imageView);
         wallpaper = w.url;
 
@@ -94,6 +115,7 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.wall
         public wallpaperViewHolder(View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.image_view);
+            rotateLoading = itemView.findViewById(R.id.rotateloading);
             itemView.setOnClickListener(this);
 
 
