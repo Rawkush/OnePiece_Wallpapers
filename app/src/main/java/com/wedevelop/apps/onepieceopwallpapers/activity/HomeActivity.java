64c10c3c261fc5c11885325a8a81075d24db631d
Ahraar <ahraralam2012@gmail.com>
2018-09-02 00:39:29 +0530

    Alert Dialog added

--- app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/HomeActivity.java
+++ app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/activity/HomeActivity.java
@@ -3,6 +3,7 @@ package com.wedevelop.apps.onepieceopwallpapers.activity;
 
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -12,16 +13,13 @@ import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.MobileAds;
-import com.shashank.sony.fancydialoglib.Animation;
-import com.shashank.sony.fancydialoglib.FancyAlertDialog;
-import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
-import com.shashank.sony.fancydialoglib.Icon;
 import com.wedevelop.apps.onepieceopwallpapers.HintServiceImpl;
 import com.wedevelop.apps.onepieceopwallpapers.R;
 import com.wedevelop.apps.onepieceopwallpapers.adapter.TabLayoutAdapter;
@@ -31,15 +29,15 @@ public class HomeActivity extends AppCompatActivity {
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
     TabLayoutAdapter adapter;
-
+    boolean doubleTap;
+    View download, search, newtab, categorytab, favtab;
     private TabLayout tabLayout;
     private int[] tabIcons = {
             R.drawable.ic_tab_new,
             R.drawable.ic_random,
             R.drawable.ic_favorite
     };
-    boolean doubleTap;
-    View download, search, newtab, categorytab, favtab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +69,6 @@ public class HomeActivity extends AppCompatActivity {
 
         }
 
-        PopUp();
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -95,31 +92,77 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-
     private void setupTabIcons() {
         tabLayout.getTabAt(0).setIcon(tabIcons[0]);
         tabLayout.getTabAt(1).setIcon(tabIcons[1]);
         tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
+
     private void setupViewPager(ViewPager viewPager) {
         adapter = new TabLayoutAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
     }
 
     public void onBackPressed() {
-        if (doubleTap) {
-            super.onBackPressed();
-        } else {
-            Toast.makeText(this, "Please Back again to Exit", Toast.LENGTH_SHORT).show();
-            doubleTap = true;
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    doubleTap = false;
-                }
-            }, 1000);
-        }
+        ExitApp();
+    }
+
+
+    //DialogBox Main Function
+    public void ExitApp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                HomeActivity.this);
+
+        builder.setTitle("Exit?");//Title
+        builder.setMessage("If you love the One Piece Wallpapers,please rate us");//Message
+        builder.setIcon(R.drawable.app_icon);
+        builder.setCancelable(false);
+
+
+        //Negative Message
+        builder.setNegativeButton("RATE US",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                        // To count with Play market backstack, After pressing back button,
+                        // to taken back to our application, we need to add following flags to intent.
+                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                        try {
+                            startActivity(goToMarket);
+                        } catch (ActivityNotFoundException e) {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                        }
+
+
+                    }
+                });
+
+
+        builder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        //Positive Message
+        builder.setPositiveButton("EXIT",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        /* close this activity
+                         *  When Exit is clicked
+                         */
+                        finish();
+                    }
+                });
+
+        builder.show();
     }
 
     public void setTabsandShowHints() {
@@ -134,55 +177,6 @@ public class HomeActivity extends AppCompatActivity {
         hintService.showHint(this);
 
     }
-
-    public void PopUp() {
-        new FancyAlertDialog.Builder(this)
-                .setTitle("Rate us if you like the app")
-                .setBackgroundColor(Color.parseColor("#000000"))  //Don't pass R.color.colorvalue
-                .setMessage("Do you really want to Exit ?")
-                .setNegativeBtnText("Cancel")
-                .setPositiveBtnBackground(Color.parseColor("#FF4081"))  //Don't pass R.color.colorvalue
-                .setPositiveBtnText("Rate")
-                .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  //Don't pass R.color.colorvalue
-                .setAnimation(Animation.POP)
-                .isCancellable(true)
-                .setIcon(R.drawable.ic_star_border_black_24dp, Icon.Visible)
-                .OnPositiveClicked(new FancyAlertDialogListener() {
-                    @Override
-                    public void OnClick() {
-                        String playStoreMarketUrl = "market://details?id=";
-                        String playStoreWebUrl = "https://play.google.com/store/apps/details?id=";
-                        String packageName = getPackageName();
-                        try {
-                            Intent intent = getApplicationContext()
-                                    .getPackageManager()
-                                    .getLaunchIntentForPackage("com.android.vending");
-                            if (intent != null) {
-                                ComponentName androidComponent = new ComponentName("com.android.vending",
-                                        "com.wedevelop.apps.onepieceopwallpapers.activity");
-                                intent.setComponent(androidComponent);
-                                intent.setData(Uri.parse(playStoreMarketUrl + packageName));
-                            } else {
-                                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(playStoreMarketUrl + packageName));
-                            }
-                            startActivity(intent);
-                        } catch (ActivityNotFoundException e) {
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(playStoreWebUrl + packageName));
-                            startActivity(intent);
-                        }
-                        // Toast.makeText(getApplicationContext(),"Rate",Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .OnNegativeClicked(new FancyAlertDialogListener() {
-                    @Override
-                    public void OnClick() {
-                        //Toast.makeText(getApplicationContext(),"Cancel",Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .build();
-    }
-
-
 
 
 }
