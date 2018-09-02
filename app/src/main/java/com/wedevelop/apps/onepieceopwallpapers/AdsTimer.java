21ac86170173d55a617ec259c7367bb9bfc0ff8c
rawkush <ankushrawat950@gmail.com>
2018-09-02 22:37:25 +0530

    added timer for ads

---
+++ app/src/main/java/com/wedevelop/apps/onepieceopwallpapers/AdsTimer.java
@@ -0,0 +1,58 @@
+package com.wedevelop.apps.onepieceopwallpapers;
+
+import android.os.CountDownTimer;
+import android.view.View;
+
+public class AdsTimer {
+
+    CountDownTimer countDownTimer;
+    static boolean showAds=true;
+
+
+    public void controlTimer(int time){
+
+            countDownTimer=  new CountDownTimer( time*1000 + 100, 1000) {  // first param is timer till which to count
+                // second param is in what intervals should it count
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // after every second
+                    updateTime((int) (millisUntilFinished / 1000));
+
+
+                }
+
+                @Override
+                public void onFinish() {
+                    // when conter is finished(after 10 sec)
+                    resetTimer();
+                    showAds=true;
+                    //TODO: display ads
+
+                }
+            }.start();
+
+
+    }
+
+
+    /*
+
+      updating time here*
+
+    */
+
+  public void updateTime(int secondsLeft){
+
+        int minutes =(int) secondsLeft/ 60;
+        int seconds= secondsLeft-minutes*60;
+        //timerTextView.setText(Integer.toString(minutes)+":"+Integer.toString(seconds));
+    }
+
+    public void resetTimer(){
+        countDownTimer.cancel();
+      //  timerTextView.setVisibility(View.GONE);
+  }
+
+
+
+}
